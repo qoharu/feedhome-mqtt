4603715d99428df5551fccde12704d6f0f8133eb
squhart <squhart@gmail.com>
2017-08-15 00:08:41 +0700

    blah

---
+++ index.js
@@ -0,0 +1,25 @@
+var mosca = require('mosca')
+
+var moscaSettings = {
+  port: 1883,
+  persistence: {
+    factory: mosca.persistence.Memory
+  }
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+	console.log('client connected', client.id);		
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+var mqtt = require('mqtt')
+
+var client = mqtt.connect('mqtt://localhost:1883');
+client.on('connect', function(){
+    console.log('client connected');
+});
+
+
+var m = {type:"Fiat", model:"500", payload:"white"};
+client.publish('presence', JSON.stringify(m));
